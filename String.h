9321cf4f02633a6755dbe95daaaf1079c56bc366
Vitalya Shevelev <lords-2012@mail.ru>
2022-01-22 13:08:53 +0500

    String() для строки

--- String.h
+++ String.h
@@ -1,9 +1,19 @@
 #pragma once
-
+#include <cstring>
+#include <string>
 class String
 {
-	char* str = __nullptr;
+	char* str = nullptr;
 public:
 	String() {}
+    String(const char* str) {
+		int size = strlen(str);
+		this->str = new char[size + 1];
+		strcpy_s(this->str, size + 1, str); 
+	}
+
+	~String() {
+		delete[] str;
+	}
 };
 
